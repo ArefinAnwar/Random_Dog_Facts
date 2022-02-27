c489c6338c6d6804087efe8b3e0af97fa6f4dae1
Arefin Anwar <arefinanwar112@gmail.com>
2022-02-28 00:48:18 +0600

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+const port = 8080;
+const d = require('./data.json');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+app.listen(port, () => { console.log(d.length) });
+
+app.get('/dog-facts', (req, res) => {
+    res.status(200).send({
+        "fact": d[getRandomInt(d.length)],
+    });
+});
